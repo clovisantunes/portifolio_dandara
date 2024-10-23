d274f3f04a5a8cb2a53f4ebec1178705de1b08d0
unknown <clovissantannaa@gmail.com>
2024-10-23 19:39:18 -0300

    Fix copy

--- src/app/components/copy/index.tsx
+++ src/app/components/copy/index.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
 import styles from './styles.module.scss';
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            Copyright © DevRoom · 2024
+            <Link href="https://devroom.tech/" target="_blank" rel="noopener noreferrer">
+                <span>
+                Copyright © DevRoom · 2024
+                </span>
+            </Link>
         </footer>
     );
 }
